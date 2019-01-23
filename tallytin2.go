bc37c3d306a1d4fe5968be5b850caf2e4fec8165
Teppo Rekola <sytem@iki.fi>
2019-01-23 19:39:06 +0200

    other channel

--- tallytin2.go
+++ tallytin2.go
@@ -8,11 +8,9 @@ import (
 	"fmt"
 	"time"
 
-	"net/http"
-
 	"github.com/kidoman/embd"
-
 	_ "github.com/kidoman/embd/host/all"
+	"net/http"
 )
 
 func main() {
@@ -23,7 +21,8 @@ func main() {
 	}
 	defer embd.CloseGPIO()
 
-	tally1, err := embd.NewDigitalPin(4)
+	// first input
+	tally1, err := embd.NewDigitalPin(02)
 	if err != nil {
 		panic(err)
 	}
@@ -33,23 +32,43 @@ func main() {
 		panic(err)
 	}
 	tally1.ActiveLow(false)
-
+	tally1.PullUp()
+	
 	err = tally1.Watch(embd.EdgeBoth, handleTally1)
 	if err != nil {
 		panic(err)
 	}
 
+	//second input
+	tally2, err := embd.NewDigitalPin(04)
+	if err != nil {
+		panic(err)
+	}
+	defer tally2.Close()
+
+	if err := tally2.SetDirection(embd.In); err != nil {
+		panic(err)
+	}
+	tally2.ActiveLow(false)
+	tally2.PullUp()
+
+	err = tally2.Watch(embd.EdgeBoth, handleTally2)
+	if err != nil {
+		panic(err)
+	}
+
 	fmt.Printf("start")
-	for true {
+	for {
 		time.Sleep(10000 * time.Millisecond)
 		fmt.Println("ping")
 	}
 
 }
+
 func handleTally1(pin embd.DigitalPin) {
 	pinValue, _ := pin.Read()
 	if pinValue == 0 {
-		fmt.Println("cut in")
+		fmt.Println("1 cut in")
 		resp, err := http.Get("http://10.39.1.85:8088/api/?Function=MultiViewOverlayOn&Value=1&Input=6")
 		if err != nil {
 			panic(err)
@@ -57,8 +76,30 @@ func handleTally1(pin embd.DigitalPin) {
 		resp.Body.Close()
 
 	} else {
-		fmt.Println("cut out")
+		fmt.Println("1 cut out")
 		resp, err := http.Get("http://10.39.1.85:8088/api/?Function=MultiViewOverlayOff&Value=1&Input=6")
+		if err != nil {
+			panic(err)
+		}
+		resp.Body.Close()
+	}
+
+	time.Sleep(100 * time.Millisecond)
+}
+
+func handleTally2(pin embd.DigitalPin) {
+	pinValue, _ := pin.Read()
+	if pinValue == 0 {
+		fmt.Println("2 cut in")
+		resp, err := http.Get("http://10.39.1.85:8088/api/?Function=Transition1")
+		if err != nil {
+			panic(err)
+		}
+		resp.Body.Close()
+
+	} else {
+		fmt.Println("2 cut out")
+		resp, err := http.Get("http://10.39.1.85:8088/api/?Function=Transition2")
 		if err != nil {
 			panic(err)
 		}
